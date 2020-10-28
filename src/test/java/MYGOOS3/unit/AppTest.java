57c6aa11b367c07a8ee3e750daf687b7d2ee7a5b
Dan <dankim0213@gmail.com>
2020-10-28 21:13:46 +0900

    learned why junit must be used with Hamcrest's Matchers

--- src/test/java/MYGOOS3/unit/AppTest.java
+++ src/test/java/MYGOOS3/unit/AppTest.java
@@ -1,15 +1,33 @@
 package MYGOOS3.unit;
 
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import org.hamcrest.core.StringContains;
+
+import static org.hamcrest.core.StringContains.containsString;
+
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
+ * This class for unit test is supposed to figure out 
+ * the difference between junit only and junit with hamcrest. 
+ * 
+ * The result is much significant with respect to seeking problems. 
  */
-class AppTest {
+public class AppTest {
     /**
      * Rigorous Test.
      */
     @Test
-    void testApp() {
+    public void testApp() {
+        assertTrue("this is a test ", 1 == 2);
+    }
+
+    @Test
+    public void testAppWithHamcrest() {
+        String a = "I want some banana";
+        assertThat("This is a test with Hamcrest", a, containsString("banna"));
     }
 }
